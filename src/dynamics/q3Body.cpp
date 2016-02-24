9ca70b27b5858798276a74b34ae954df3885a2fe
cbush <cbush@macadamian.com>
2016-02-24 00:08:36 -0500

    Clean up xcproject warnings and remove unused variables

--- src/dynamics/q3Body.cpp
+++ src/dynamics/q3Body.cpp
@@ -407,7 +407,6 @@ void* q3Body::GetUserdata( ) const {
 //--------------------------------------------------------------------------------------------------
 void q3Body::Render( q3Render* render ) const
 {
-	q3Transform tx = m_tx;
 	bool awake = IsAwake( );
 	q3Box* box = m_boxes;
 
